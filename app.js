85babbaf15d175e17b8624100bd553df95c4118c
Parul Dudeja <paruldudeja93@gmail.com>
2022-08-03 17:40:10 +0400

    rgb to hex

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 "use strict";
+// function from rgb value into hex
+function hex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+console.log(hex(205, 109, 42));
