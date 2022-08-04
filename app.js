0f525ecb1ddf39742a5bc655857f13cc5ed46891
Parul Dudeja <paruldudeja93@gmail.com>
2022-08-04 14:24:05 +0400

    new color code

--- app.js
+++ app.js
@@ -3,4 +3,19 @@
 function hex(r, g, b) {
   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
-console.log(hex(205, 109, 42));
+// console.log(hex(205, 109, 42));
+function makeColor(r, g, b) {
+  const color = {};
+  color.r = r;
+  color.g = g;
+  color.b = b;
+  color.rgb = function () {
+    //desturing the this keyword
+    const { r, g, b } = this;
+    return `rgb(${r},${g},${b})`;
+  };
+  return color;
+}
+
+const firstColor = makeColor(45, 12, 32);
+firstColor.rgb;
