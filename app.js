81e6596a1f4f6c001b8bdb5af7f6556826bd3f8d
Parul Dudeja <paruldudeja93@gmail.com>
2022-08-08 16:00:30 +0400

    rgba prototype

--- app.js
+++ app.js
@@ -39,6 +39,10 @@ Color.prototype.hex = function () {
   const { r, g, b } = this;
   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 };
+Color.prototype.rgba = function (a = 1.0) {
+  const { r, g, b } = this;
+  return `rgb(${r}, ${g}, ${b},${a})`;
+};
 
 const color1 = new Color(40, 50, 60);
 const color2 = new Color(10, 20, 30);
