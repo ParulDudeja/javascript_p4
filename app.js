41abcfaa48299ca09bb6472b39f4bd70ba8d852e
Parul Dudeja <paruldudeja93@gmail.com>
2022-08-08 15:52:14 +0400

    color variables

--- app.js
+++ app.js
@@ -34,3 +34,6 @@ Color.prototype.rgb = function () {
   const { r, g, b } = this;
   return `rgb(${r}, ${g}, ${b})`;
 };
+
+const color1 = new Color(40, 50, 60);
+const color2 = new Color(10, 20, 30);
